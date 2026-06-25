277967e868551eea6bb98bccd09ff0e29b5be636
limeassistentevirtual-spec <limeassistentevirtual@gmail.com>
2026-06-25 18:44:56 -0300

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://agsrcabypcjbviikmqdl.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_LJ5HL78iPlgYuo5D0L5TTw_rLDmX9Ba';
+
+window.limeSupabase = supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
